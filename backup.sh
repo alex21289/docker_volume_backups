72a31f76cfe065fa30ad9c54262b06c24f43d9fc
Alex Joedt <alex@h2879013.stratoserver.net>
2021-12-27 17:33:25 +0100

    first commit

---
+++ backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# source: https://stackoverflow.com/questions/26331651/how-can-i-backup-a-docker-container-with-its-data-volumes
+# This script allows you to backup a single volume from a container
+# Data in given volume is saved in the current directory in a tar archive.
+CONTAINER_NAME=$1
+VOLUME_NAME=$2
+DESTINATION=$3
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+usage() {
+  echo "Usage: $0 [container name] [volume name] [backup destination]"
+  exit 1
+}
+
+if [ -z $CONTAINER_NAME ]
+then
+  echo "Error: missing container name parameter."
+  usage
+fi
+
+if [ -z $VOLUME_NAME ]
+then
+  echo "Error: missing volume name parameter."
+  usage
+fi
+
+if [ -z $DESTINATION ]
+then
+  echo "Error: missing destination parameter."
+  usage
+fi
+
+sudo docker run --rm --volumes-from $CONTAINER_NAME -v $DESTINATION:/backup busybox tar cvzf /backup/backup_$DATE.tar.gz $VOLUME_NAME
